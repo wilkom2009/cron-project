780b507f3e2d922a6ae37b1947771f4fbdd96e1b
Wilson KOMLAN <koffikomlan01@gmail.com>
2024-10-29 22:31:43 +0100

    Testing filtering before processing

--- src/main/java/com/wilkom/cronproject/batch/MyProcessor.java
+++ src/main/java/com/wilkom/cronproject/batch/MyProcessor.java
@@ -14,6 +14,11 @@ public class MyProcessor implements ItemProcessor<RawData, Account> {
     @Override
     public Account process(final RawData data) throws Exception {
 
+        // Filter out accounts with names longer than 10 characters
+        if (data.getName().length() > 10) {
+            return null;
+        }
+
         return new Account(data.getId(), data.getName().toUpperCase(),
                 data.getAmount(), LocalDateTime.now());
     }
