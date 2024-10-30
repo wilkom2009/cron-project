3c8aa800f388b64c6c437172f2f1d6b9686906d2
Wilson KOMLAN <koffikomlan01@gmail.com>
2024-10-30 22:58:07 +0100

    Reading file from S3

--- src/main/java/com/wilkom/cronproject/batch/BatchConfig.java
+++ src/main/java/com/wilkom/cronproject/batch/BatchConfig.java
@@ -1,21 +1,29 @@
 package com.wilkom.cronproject.batch;
 
+import java.io.InputStream;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.wilkom.cronproject.model.Account;
 import com.wilkom.cronproject.model.RawData;
 
@@ -25,8 +33,14 @@ import jakarta.annotation.Nonnull;
 @EnableBatchProcessing
 public class BatchConfig {
 
+    private static final String bucketName = "ws-cron-job-bucket";
+    private static final String key = "data-list.csv";
+
     @Value("${file.input}")
     private String fileInput;
+
+    @Autowired
+    private AmazonS3 amazonS3Client;
 
     @Bean
     public Tasklet myTasklet() {
@@ -34,14 +48,25 @@ public class BatchConfig {
     }
 
     @Bean
+    @StepScope // create a new instance for each step execution
+    @Nonnull
     public FlatFileItemReader<RawData> reader() {
-        return new FlatFileItemReaderBuilder<RawData>().name("rawItemReader")
-                .resource(new ClassPathResource(fileInput))
-                .delimited()
-                .names("id", "name", "amount")
-                .targetType(
-                        RawData.class)
-                .build();
+        FlatFileItemReader<RawData> reader = new FlatFileItemReader<>();
+        reader.setResource(new InputStreamResource(getInputStream(bucketName, key)));
+        reader.setLinesToSkip(1); // Skip header row if present
+
+        DefaultLineMapper<RawData> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id", "name", "amount");
+
+        BeanWrapperFieldSetMapper<RawData> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(RawData.class);
+
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        reader.setLineMapper(lineMapper);
+
+        return reader;
     }
 
     @Bean
@@ -73,4 +98,8 @@ public class BatchConfig {
         return new MyWriter();
     }
 
+    public InputStream getInputStream(String bucketName, String key) {
+        S3Object s3Object = amazonS3Client.getObject(new GetObjectRequest(bucketName, key));
+        return s3Object.getObjectContent();
+    }
 }
---
+++ src/main/java/com/wilkom/cronproject/config/S3Config.java
@@ -0,0 +1,20 @@
+package com.wilkom.cronproject.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
